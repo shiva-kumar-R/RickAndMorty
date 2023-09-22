8bae9e43e56478550497636769fd9198b35ea171
Shivakumar R <shivakumar.r@nocodez.com>
2023-09-22 22:51:20 +0530

    Update character status and icon based on STATUS

--- app/src/main/java/com/example/rickandmorty/adapter/CharacterViewHolder.kt
+++ app/src/main/java/com/example/rickandmorty/adapter/CharacterViewHolder.kt
@@ -2,15 +2,25 @@ package com.example.rickandmorty.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.rickandmorty.R
 import com.example.rickandmorty.model.Character
 import com.example.rickandmorty.databinding.ItemCharacterBinding
+import com.example.rickandmorty.model.Status
+import com.google.android.material.textview.MaterialTextView
 
 class CharacterViewHolder(private val binding: ItemCharacterBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: Character) = with(binding) {
-            ivAvatar.load(item.image)
-            tvName.text = item.name
-            tvStatus.text = item.status.toString()
-        }
+    fun bind(item: Character) = with(binding) {
+        ivAvatar.load(item.image)
+        tvName.text = item.name
+        tvStatus.text = item.status.toString()
+        tvStatus.setDrawable(item.status)
+    }
+
+    private fun MaterialTextView.setDrawable(status: Status) = when (status) {
+        Status.ALIVE -> compoundDrawables[0]?.setTint(resources.getColor(R.color.green_a700, null))
+        Status.DEAD -> compoundDrawables[0]?.setTint(resources.getColor(R.color.red_a700, null))
+        Status.UNKNOWN -> compoundDrawables[0]?.setTint(resources.getColor(R.color.gray_700, null))
+    }
 }
